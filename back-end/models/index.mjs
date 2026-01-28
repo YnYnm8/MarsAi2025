4bea28f442688fe56da8065450cd19a8c497b1d8
Luz Rivera <luzrivera154@hotmail.com>
2026-01-28 14:00:09 +0100

    bug-user-bdd

--- back-end/models/index.mjs
+++ back-end/models/index.mjs
@@ -1,6 +1,7 @@
 import { Film } from "./film.mjs";
 import { Playlist } from "./playlist.mjs";
 import { PlaylistFilm } from "./playlist_films.mjs";
+import { User } from "./user.mjs";
 
 // MANY TO MANY
 Playlist.belongsToMany(Film, {
@@ -17,4 +18,5 @@ export {
   Film,
   Playlist,
   PlaylistFilm,
+  User
 };
