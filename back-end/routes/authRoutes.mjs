bf9d4b5bce14aad9a4c31c5799f52696898747a4
Jonathan S <jonset31@hotmail.com>
2026-01-28 13:50:44 +0100

    auth system

---
+++ back-end/routes/authRoutes.mjs
@@ -0,0 +1,16 @@
+import express from "express"
+import { register } from "../controllers/authController.mjs";
+import { login } from "../controllers/authController.mjs";
+import { logout } from "../controllers/authController.mjs";
+
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+
+
+
+
+export default router
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,25 @@
 // import { useState } from 'react'
 import './App.css'
-import{Routes,Route} from "react-router";
+import { Routes, Route } from "react-router";
 import Film from './pages/auth/film';
+import { Login } from './components/pages/auth/login';
+import { Register } from './components/pages/auth/register';
+import Logout from './components/pages/auth/logout'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-    <section>
-    <main>
-      <Routes>
-         <Route path="/film" element={<Film />} />
-      </Routes>
-    </main>
-      </section> 
+      <section>
+        <main>
+          <Routes>
+            <Route path="/film" element={<Film />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </main>
+      </section>
     </>
   )
 }
