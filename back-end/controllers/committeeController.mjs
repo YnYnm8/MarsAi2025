e628a1a0e605ebaf969874fa334279af2e1c0476
Noune <nathaliecamiul@yahoo.fr>
2026-01-30 15:25:15 +0100

    vue d'ensemble

--- back-end/controllers/committeeController.mjs
+++ back-end/controllers/committeeController.mjs
@@ -1,37 +1,37 @@
-// controllers/committeeController.mjs
+// // controllers/committeeController.mjs
 
-export const getComite = (req, res) => {
-  res.json("Comite router");
-};
+// export const getComite = (req, res) => {
+//   res.json("Comite router");
+// };
 
-export const selectFilm = async (req, res) => {
+// export const selectFilm = async (req, res) => {
 
-    // const lignes = await Model.findAll();
-    const films = await Film.findAll();
+//     // const lignes = await Model.findAll();
+//     const films = await Film.findAll();
 
-  res.json(films);
-};
+//   res.json(films);
+// };
 
-export const addNote = async (req, res) => {
+// // export const addNote = async (req, res) => {
 
-    const
-  res.json({});
-};
+// //     const
+// //   res.json({});
+// // };
 
-export const validateFilm = (req, res) => {
-  const { filmid } = req.params;
-  res.json({ filmid });
-};
+// export const validateFilm = (req, res) => {
+//   const { filmid } = req.params;
+//   res.json({ filmid });
+// };
 
-export const refuseFilm = (req, res) => {
-  const { filmid } = req.params;
-  res.json({ filmid });
-};
+// export const refuseFilm = (req, res) => {
+//   const { filmid } = req.params;
+//   res.json({ filmid });
+// };
 
-export const listFilms = (req, res) => {
-  res.json({});
-};
+// export const listFilms = (req, res) => {
+//   res.json({});
+// };
 
-export const sortHistory = (req, res) => {
-  res.json({});
-};
+// export const sortHistory = (req, res) => {
+//   res.json({});
+// };
