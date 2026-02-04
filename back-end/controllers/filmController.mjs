90bb85667fbe8156e426b004ba5753f6c21d66de
Luz Rivera <luzrivera154@hotmail.com>
2026-02-04 17:03:29 +0100

    Creation dossier helpers avec function de Meiko catch error + routes films + controllers films + no pagination + no test + no auth + no filtres

--- back-end/controllers/filmController.mjs
+++ back-end/controllers/filmController.mjs
@@ -1,96 +1,218 @@
-
-import { Film } from "../models/film.mjs";
-// import { Candidature }
-// import { User }
-
-const sendErrors = (res, errors, status = 400) => {
-    // 500系エラーだけログに出す
-    if (status === 500) {
-        console.error(errors);
-    }
-
-    return res.status(status).json({ errors });
-};
-
-function catchError(res, err) {
-
-    if (err.name === "SequelizeValidationError") {
-        const errors = err.errors.map((e) => ({
-            field: e.path,
-            message: e.message,
-        }));
-        return sendErrors(res, errors, 400);
-
-    }
-    return sendErrors(res, [{ field: "global", message: err.message }], 500);
-
-}
+import Film from "../models/Films.mjs";
+import User from "../models/User.mjs";
+import File from "../models/File.mjs";
+import Selection from "../models/Selection.mjs";
+import { catchError } from "../helpers/errorHandler.mjs";
 
 
-export async function getAllFilms(req, res) {
+// Recuperation des tous les films
+
+export async function getFilms(req, res) {
     try {
-        const filmsData = await Film.findAll({
+        const FilmData = await Film.findAll({
             include: [
                 {
-                    model: Candidature,
-                    attributes: ["id"],
-
-                },
-                {
-                    model: User,
-                    attributes: ["username"]
+                    model: File,
+                    attributes: ['id', 'film_url', 'poster_url', 'subtitle', 'outil_Ai']
                 }
-            ]
+            ],
+            order: [['createdAt', 'DESC']]
         });
-
-        if (!filmsData) {
-            return res.status(404).json({ message: "Film introuvable" });
+        if (FilmData.length == 0) {
+            return res.status(200).json({
+                message: "Aucun film disponible",
+                data: [],
+            });
         }
-
-        return res.status(200).json(filmsData);
+        return res.status(200).json(FilmData);
     } catch (err) {
         return catchError(res, err)
     }
 }
+// Recuperation d'un film par son id
 
 export async function getFilmById(req, res) {
     try {
-        const filmData = await Film.findByPk(req.params.id, {
+        const id = Number(req.params.id);
+
+        if (!id || isNaN(id)) {
+            return res.status(400).json({
+                message: "ID de film invalide",
+            });
+        }
+
+        const FilmData = await Film.findByPk(id, {
             include: [
                 {
-                    model: Candidature,
-                    attributes: ["id"],
-
+                    model: User,
+                    attributes: ['id', 'firstName', 'lastName', 'email']
                 },
                 {
-                    model: User,
-                    attributes: ["username"]
+                    model: File,
+                    attributes: ['id', 'film_url', 'poster_url', 'subtitle', 'outil_Ai']
                 }
             ]
         });
-        if (!filmData) {
-            return res.status(200).json({ message: "Film introvable " });
+        if (!FilmData) {
+            return res.status(404).json({
+                message: "Film non trouvé",
+            });
         }
-        return res.status(200).json(filmData);
+        return res.status(200).json(FilmData);
     } catch (err) {
         return catchError(res, err)
     }
 }
-export async function getFilmsByStatus(req, res) {
-    try {
-        const { status } = req.params;
-        const allowedStatus = ["accepted", "rejected", "to_discuss"];
-        if (!allowedStatus.includes(status)) {
-            return res.status(400).json({ message: "Invalid status" });
-        }
 
-        const films = await Film.findAll({
-            where: { status },
-            include: filmInclude
+export async function getFilmsSelect(req, res) {
+    try {
+        const selectData = await Selection.findAll({
+            include: [
+                {
+                    model: Film,
+                    attributes: ['id', 'user_id', 'title', 'collaborateur', 'description',
+                        'school', 'country', 'duration', 'title', 'category', 'generate_Ai', 'socialNetworks'],
+                    include: [
+                        {
+                            model: File,
+                            attributes: ['id', 'subtitle', 'film_url', 'poster_url']
+                        }
+                    ]
+                }
+            ]
+        });
+        if (!selectData || selectData.length === 0) {
+            return res.status(200).json({
+                message: "Aucune sélection de films disponible",
+                data: []
+            });
+        }
+        return res.status(200).json(selectData);
+    } catch (err) {
+        return catchError(res, err)
+    }
+}
+
+// Creation d'un nouveau film
+export async function createFilm(req, res) {
+    try {
+        const { last_name, collaborateur, email, school, country, bio,
+            socialNetworks, title, description, duration, category, generate_Ai,
+            film_url, poster_url, subtitle, outil_Ai
+        } = req.body;
+
+        const newFilm = await Film.create({
+            user_id: req.user.id,
+            last_name,
+            collaborateur,
+            email,
+            school,
+            country,
+            bio,
+            socialNetworks,
+            title,
+            description,
+            duration: parseFloat(duration),
+            category,
+            generate_Ai,
+
         });
 
-        return res.status(200).json(films);
+        if (film_url || poster_url) {
+            await File.create({
+                film_id: newFilm.id,
+                film_url,
+                poster_url,
+                subtitle: subtitle || "",
+                outil_Ai
+            });
+        }
+        const filmWithFile = await Film.findByPk(newFilm.id, {
+            include: [{
+                model: File,
+                attributes: ['id', 'film_url', 'poster_url', 'subtitle', 'outil_Ai']
+            }]
+        });
+        return res.status(201).json(filmWithFile);
     } catch (err) {
-        return catchError(res, err);
+        return catchError(res, err)
+    }
+}
+
+// Modification d'un film existant
+
+export async function updateFilm(req, res) {
+    try {
+        const { collaborateur, email, school, country, bio,
+            socialNetworks, title, description, duration, category, generate_Ai,
+            film_url, poster_url, subtitle, outil_Ai
+        } = req.body;
+
+        const filmId = parseInt(req.params.id);
+        const film = await Film.findByPk(filmId, {
+            include: File
+        });
+
+        if (!filmId || isNaN(filmId)) {
+            return res.status(400).json({ message: "Film non trouvé" });
+        }
+
+
+        await film.update(
+            collaborateur,
+            email,
+            school,
+            country,
+            bio,
+            socialNetworks,
+            title,
+            description,
+            duration,
+            category,
+            generate_Ai,
+            { where: { id: filmId } });
+
+        if (film.url || poster_url || subtitle || outil_Ai) {
+            await film.File.update({
+                film_url,
+                poster_url,
+                subtitle,
+                outil_Ai
+            });
+        }
+        const updatedFilm = await Film.findByPk(film.id, {
+            include: File
+        });
+        return res.status(200).json(updatedFilm);
+    } catch (err) {
+        return catchError(res, err)
+    }
+
+}
+
+// Suppression d'un film 
+
+export async function deleteFilm(req, res) {
+    try {
+        const filmId = parseInt(req.params.id);
+        const film = await Film.findByPk(filmId);
+
+        if (!filmId || isNaN(filmId)) {
+            return res.status(400).json({ message: "Film non trouvé" });
+        }
+
+        await film.destroy();
+        return res.status(200).json({ message: "Film supprimé avec succès" });
+    } catch (err) {
+        return catchError(res, err)
+    }
+}
+
+export async function getMyFilms(req, res) {
+    try {
+
+
+    } catch (err) {
+        return catchError(res, err)
     }
 }
--- back-end/models/Annotation.mjs
+++ back-end/models/Annotation.mjs
@@ -1,7 +1,13 @@
 import sequelize from "../config/database.mjs";
+import { DataTypes } from "sequelize";
 
-const Annotation = sequelize.define("Annotation",{
-
+const Annotation = sequelize.define("Annotation", {
+    content: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+}, {
+    timestamps: true
 });
 
 
